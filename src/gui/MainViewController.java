e34bbe12739001c00f47345de5e41b8867d845ab
root6439 <nicolas.soaresh.98@gmail.com>
2019-07-15 21:02:02 -0300

    implementação do DepartmentService

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable{
 
@@ -34,7 +35,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
@@ -78,6 +79,39 @@ public class MainViewController implements Initializable{
 		}
 	}
 	
-	
+	private synchronized void loadView2(String absoluteName) {
+		try {
+			
+			//abrir recurso do arquivo enviado como parametro
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			
+			VBox newVBox = loader.load();
+			
+			//pegar referencia da cena 
+			Scene mainScene = Main.getMainScene();
+			
+			//operação pra pegar o conteudo da tela
+			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+			
+			//pegar primeiro filho do vbox da janela principal
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			//limoar todos os filhos do main vbox
+			mainVBox.getChildren().clear();
+			
+			//adicionar o main menu
+			mainVBox.getChildren().add(mainMenu);
+			
+			//adicionar coleção, no caso os filhos da janela que se quer abrir
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+			
+		} catch (IOException e) {
+			Alerts.showAlert("Input/Output exception", "Erro carregando a página", e.getMessage(), AlertType.ERROR);
+		}
+	}
 
 }
