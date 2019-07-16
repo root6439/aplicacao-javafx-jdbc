ffb9565b1fb3efbbc56dc03beb76bbbbbcf62463
root6439 <nicolas.soaresh.98@gmail.com>
2019-07-15 22:00:15 -0300

    Ação de inicialização como parâmetro

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,12 +36,15 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	@Override
@@ -48,7 +52,7 @@ public class MainViewController implements Initializable{
 	}
 	
 	
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			
 			//abrir recurso do arquivo enviado como parametro
@@ -74,40 +78,8 @@ public class MainViewController implements Initializable{
 			//adicionar coleção, no caso os filhos da janela que se quer abrir
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-		} catch (IOException e) {
-			Alerts.showAlert("Input/Output exception", "Erro carregando a página", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	private synchronized void loadView2(String absoluteName) {
-		try {
-			
-			//abrir recurso do arquivo enviado como parametro
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			
-			VBox newVBox = loader.load();
-			
-			//pegar referencia da cena 
-			Scene mainScene = Main.getMainScene();
-			
-			//operação pra pegar o conteudo da tela
-			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
-			
-			//pegar primeiro filho do vbox da janela principal
-			Node mainMenu = mainVBox.getChildren().get(0);
-			
-			//limoar todos os filhos do main vbox
-			mainVBox.getChildren().clear();
-			
-			//adicionar o main menu
-			mainVBox.getChildren().add(mainMenu);
-			
-			//adicionar coleção, no caso os filhos da janela que se quer abrir
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-			
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
+			 T controller = loader.getController();
+			 initializingAction.accept(controller);
 			
 		} catch (IOException e) {
 			Alerts.showAlert("Input/Output exception", "Erro carregando a página", e.getMessage(), AlertType.ERROR);
